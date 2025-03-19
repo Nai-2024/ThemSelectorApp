9824cc05e25e87cff1854d8d038fe1ad779e4257
Din <Din.Salehy@triosstudent.com>
2025-03-19 09:18:45 -0400

    Initial Commit

---
+++ ThemSelectorApp/ThemSelectorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemSelectorAppApp.swift
+//  ThemSelectorApp
+//
+//  Created by Din Salehy on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct ThemSelectorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
